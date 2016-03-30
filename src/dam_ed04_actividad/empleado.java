d1d83dc439313627cfe8298b2f8648e53f46f108
Marcos Medina Castellano <Marcos Medina Castellano>
2016-03-30 18:18:11 +0100

    Tercer Commit

---
+++ src/dam_ed04_actividad/empleado.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dam_ed04_actividad;
+
+/**
+ *
+ * @author Marcos
+ */
+public class empleado {
+    //Creacion de los atributos
+    String nombre;
+    String apellidos;
+    String dni;
+    
+}
