a7841f8469933f9ae7f237dfedf9c6abdc4ba392
Marcos Medina Castellano <Marcos Medina Castellano>
2016-03-31 18:57:15 +0100

    Commit Javadoc Empleado

---
+++ src/dam_ed04_actividad/empleado.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dam_ed04_actividad;
+
+/**
+ *
+ * @author Marcos
+ */
+public class empleado {
+    String nombre;
+    String apellido;
+    String dni;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+    
+}
