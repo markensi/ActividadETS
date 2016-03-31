3e9d3bdb47818bcf06c6c344bc8d9117c20841c5
Marcos Medina Castellano <Marcos Medina Castellano>
2016-03-31 02:16:24 +0100

    Sexto Commit

--- src/dam_ed04_actividad/Banquero.java
+++ src/dam_ed04_actividad/Banquero.java
@@ -13,4 +13,29 @@ public class Banquero {
     String nombre;
     String apellido;
     int codBanquero;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public int getCodBanquero() {
+        return codBanquero;
+    }
+
+    public void setCodBanquero(int codBanquero) {
+        this.codBanquero = codBanquero;
+    }
+    
 }
