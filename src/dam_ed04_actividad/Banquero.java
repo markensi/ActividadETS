759909eb566299cd21ebcaddd282e80f74e25633
Marcos Medina Castellano <Marcos Medina Castellano>
2016-03-31 02:13:10 +0100

    Quinto Commit

---
+++ src/dam_ed04_actividad/Banquero.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dam_ed04_actividad;
+
+/**
+ *
+ * @author Marcos
+ */
+public class Banquero {
+    String nombre;
+    String apellido;
+    int codBanquero;
+}
---
+++ src/dam_ed04_actividad/empleado.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dam_ed04_actividad;
+
+/**
+ *
+ * @author Marcos
+ */
+public class empleado {
+    //Creacion de los atributos
+    String nombre;
+    String apellidos;
+    String dni;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+    
+}
